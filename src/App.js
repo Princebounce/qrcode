3a18c3962fcc5da4e0903c905455c21f8a25b34b
Lawal Taofik <layomide81@gmail.com>
2023-08-19 02:22:22 +0100

    logic edit

--- src/App.js
+++ src/App.js
@@ -1,37 +1,63 @@
-import "./App.scss";
-import React, { useState } from "react";
-import QRcode from 'qrcode'
+import React, { useEffect, useState } from "react";
+import QRcode from "qrcode";
+import AnimatedLetters from "./Animatedletters";
 
 function App() {
+  const [url, setUrl] = useState("");
+  const [qrcode, setQrcode] = useState("");
 
-    const [url, setUrl] =useState('')
-    const[qrcode, setQrcode] = useState('')
+  const [letterClass, setLetterClass] = useState("text-animate");
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass("text-animate-hover");
+    }, 3000);
+  }, []);
 
-    const GenerateQRcode = () => {
-      QRcode.toDataURL(url, {width:800, margin: 2}, (err, url) => {
-        if (err) return console.error(err)
+  const GenerateQRcode = () => {
+    QRcode.toDataURL(url, { width: 800, margin: 2 }, (err, url) => {
+      if (err) return console.error(err);
 
-        console.log(url)
-        setQrcode(url)
-
-      })
-    }
+      console.log(url);
+      setQrcode(url);
+    });
+  };
 
   return (
     <>
       <div className="app">
-        <h1>QR Code Generator</h1>
-        <input type="text" placeholder="ex: https://google.com" value={url} onChange={(evt) => setUrl(evt.target.value)} />
-        <button onClick={GenerateQRcode}>Generate</button>
+        <h1>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={"QR Code Generator".split("")}
+            idx={15}
+          />
+        </h1>
+        <input
+          type="text"
+          placeholder="ex: https://google.com"
+          value={url}
+          onChange={(evt) => setUrl(evt.target.value)}
+        />
+        <button onClick={GenerateQRcode}>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={"Generate".split("")}
+            idx={15}
+          />
+        </button>
 
-        {qrcode && <>
-        
-          <img src={qrcode} />
-          <a href={qrcode} download= "qrcode.png">Download</a>
-        
-        </>}
-
-
+        {qrcode && (
+          <>
+            <img src={qrcode} alt="qrcode" />
+            <a className="dl" href={qrcode} download="qrcode.png">
+              <AnimatedLetters
+                letterClass={letterClass}
+                strArray={"Download".split("")}
+                idx={15}
+              />
+            </a>
+          </>
+        )}
       </div>
     </>
   );
